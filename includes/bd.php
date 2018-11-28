7cb09c7781e9bf3965e8ed3062f54aa8c15c274e
Crespin Loic <loic.crespin09@gmail.com>
2018-11-28 17:29:09 +0100

    Fonctionalitée INSCRIPTION : * Génération automique aléatoire de la couleur du joueur. * Création d'un nouveau joueur automatique dans la table Joueur.
    L'inscription d'un joueur se fait de manière totalement automatique.

--- includes/bd.php
+++ includes/bd.php
@@ -45,8 +45,26 @@ function closeConnexion($link) {
 
 //Inscription et connexion
 
-//Fonction permettant de vérifier que le pseudonyme choisit par l'utilisateur est disponible au moment de l'inscription
+//Fonction permettant de générer une couleur aléatoire en hexa
+	function randomColor(){
+		$hex = '#';
+		//Create a loop.
+	foreach(array('r', 'g', 'b') as $color){
+	    //Random number between 0 and 255.
+	    $val = mt_rand(0, 255);
+	    //Convert the random number into a Hex value.
+	    $dechex = dechex($val);
+	    //Pad with a 0 if length is less than 2.
+	    if(strlen($dechex) < 2){
+	        $dechex = "0" . $dechex;
+	    }
+	    //Concatenate
+	    $hex .= $dechex;
+	}
+    return $hex;
+}
 
+//Fonction permettant de vérifier que le pseudonyme choisit par l'utilisateur est disponible au moment de l'inscription
 function checkAvailability($pseudo, $link) {
 	$req = "SELECT pseudo FROM humain WHERE pseudo = '" . $pseudo . "';";
 	$ans = executeQuery($link, $req);
@@ -54,9 +72,27 @@ function checkAvailability($pseudo, $link) {
 }
 
 //Fonction permettant d'enregistrer un nouvel utilisateur (pseudonyme, nom, prenom et mot de passe) dans la base de données
-
 function register($pseudo, $nom, $prenom, $hashPwd, $link) {
-	$req = "INSERT INTO humain (pseudo, nomJ, prenomJ, dateCreationCompte, val_hachage, Joueur_idJoueur) VALUES ('". $pseudo . "', '" . $nom . "', '" . $prenom . "', CURRENT_TIMESTAMP , '" . $hashPwd . "', 1);";
+	$req = "SELECT max(idJoueur) FROM joueur;";
+	$ans = executeQuery($link, $req);
+	foreach ($ans as $line) {
+		foreach ($line as $val) {
+			$nb = $val;
+		}
+	}
+	if ($nb == NULL) {
+		$nb = 1;
+		$req = "ALTER TABLE joueur AUTO_INCREMENT = 1;";
+		executeUpdate($link, $req);
+	} else {
+$nb++;
+}
+$color = randomColor();
+
+$req = "INSERT INTO joueur (couleur) VALUES ('" . $color ."');";
+executeUpdate($link, $req);
+
+	$req = "INSERT INTO humain (pseudo, nomJ, prenomJ, dateCreationCompte, val_hachage, Joueur_idJoueur) VALUES ('". $pseudo . "', '" . $nom . "', '" . $prenom . "', CURRENT_TIMESTAMP , '" . $hashPwd . "', '" . $nb . "');";
 	return executeUpdate($link, $req);
 }
 
@@ -99,6 +135,27 @@ function getTotalGamesPlayedMonth($link) {
 		}
 	}
 }
+
+//Partie
+//Fonction permettant de retourner la liste des joueurs engagés dans la partie
+
+function getPlayersInGame($link, $pseudo) {
+	$req = "SELECT J.pseudo FROM humain J WHERE J.pseudo != '$pseudo';";
+	$ans = executeQuery($link, $req);
+	$list = "";
+	$playable = False;
+	$errMessage = "<p>Il semble que vous soyez le seul joueur inscrit sur le jeu. Pour jouer il vous faut au moins une autre personne.</p>";
+	foreach ($ans as $line) {
+		foreach ($line as $val) {
+			$list .= "<input type='checkbox' name='player[]' value='$val'/><span class='entete'>$val</span><br/>";
+			$playable = True;
+		}
+	}
+	return ($playable ? $list : $errMessage);
+}
+
+//Jeu
+//Fonction permettant de créer une partie dans la base de données et retourner son identifiant
 
 
 
---
+++ main/partie.php
@@ -0,0 +1,60 @@
+<?php
+if (isset($_SESSION['pseudo'])) {
+
+	if (isset($_GET['err'])) {
+		switch ($_GET['err']) {
+			case "empty": {
+				echo "<div class='messageError col-md-offset-1'>Vous n'avez pas sélectionné d'adversaire !</div><br />";
+				break;
+			}
+			case "toomany": {
+				echo "<div class='messageError col-md-offset-1'>Vous devez sélectionner entre 1 et 3 adversaires !</div><br />";
+				break;
+			}
+			case "samecolor": {
+				echo "<div class='messageError col-md-offset-1'>Vous devez selectionner des couleurs différentes !</div><br />";
+				break;
+			}
+			default:
+				break;
+		}
+	}
+	?>
+
+<span class="connexion col-md-9">Configurer une nouvelle partie</span>
+
+<?php
+	echo "<div class='col-lg-9'> <form action='index.php?page=working&action=newGame' method='POST' name='partieForm'>";
+	echo "<fieldset><legend class='enteteconfigpartie'>Veuillez sélectionner vore adversaire joueurs :</legend>";
+	echo getPlayersInGame($link, $_SESSION['pseudo']) . "<br/>";
+
+	echo "<span class='connexion'>OU</span>";
+
+	echo "<fieldset><legend class='enteteconfigpartie'>Veuillez sélectionner votre adversaires ia :</legend>";
+	//echo getPlayersInGame($link, $_SESSION['pseudo']) . "<br/>";
+
+	echo "<fieldset><legend class='enteteconfigpartie'>Veuillez sélectionner le nombre de manche :</legend>";
+			echo "<div class='form-group'><select class='form-control' name='manche'>";
+			echo "<option selected disabled>Nombre de Manches</option>";
+			echo "<option value='one'>1</option>";
+	 		echo "<option value='two'>2</option>";
+	 		echo "<option value='three'>3</option>";
+	 		echo "<option value='four'>4</option>";
+			echo "</select></div>";
+
+		echo "<fieldset><legend class='enteteconfigpartie'>Veuillez sélectionner le jeu de carte souhaité :</legend>";
+			echo "<div class='form-group'><select class='form-control' name='theme'>";
+			echo "<option selected disabled>Jeux de cartes</option>";
+			echo "<option value='classique'>classique</option>";
+			echo "<option value='rustique'>rustique</option>";
+			echo "</select></div>";
+
+	echo "</fieldset><br/>";
+	echo "<button class='btn btn-success col-md-offset-4' type='submit' id='launch' name='submit' value='Lancer la partie'>Lancer la partie</button>";
+	echo "</form>";
+	echo "</div>";
+
+} else {
+	echo "<div>Pour pouvoir créer une partie vous devez être connecté</div>";
+}
+?>
